dd671651d04937a22d9bb8b4e8a6c57f9f85d0b4
jkchuong <jkchuong@gmail.com>
2020-12-19 22:08:47 +0000

    Updated book detail view

--- BookGallery/Controllers/BooksController.cs
+++ BookGallery/Controllers/BooksController.cs
@@ -11,6 +11,20 @@ namespace BookGallery.Controllers
         // Action result almost always used for action method regardless of return type
         public ActionResult Detail()
         {
+            // These are properties so need to start with capital letter.
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
+
             return View();
         }
     }
