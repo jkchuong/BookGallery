3574bf7b17c3c21ce837db6a67c7cd5e35c81668
jkchuong <jkchuong@gmail.com>
2020-12-20 17:08:08 +0000

    Updated comic book detail view to be strongly typed

--- BookGallery/Controllers/BooksController.cs
+++ BookGallery/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,21 +12,23 @@ namespace BookGallery.Controllers
         // Action result almost always used for action method regardless of return type
         public ActionResult Detail()
         {
-            // These are properties so need to start with capital letter.
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() { Name = "Dan Slott", Role = "Script"},
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils"},
+                    new Artist() { Name = "Victor Olazaba", Role = "Inks"},
+                    new Artist() { Name = "Edgar Delgado", Role = "Colors"},
+                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters"}   
+                }
             };
-
-
-            return View();
+            
+            // strongly typed view
+            return View(comicBook);
         }
     }
 }
